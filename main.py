00e63db7d9e636c89adbb01175f10a744f14e0b6
PassionSasha <sokolets.olexandr365@gmail.com>
2020-05-07 22:24:32 +0300

    zdelaly

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ bot = telebot.TeleBot('1076763292:AAE6xteIV1gqIfv3x27Ed5sIAYTraglvXDE')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Лаба 4 починилась?":
+    if message.text == "4 lab":
         bot.send_message(message.from_user.id, "Нет, перезапускай знов шарманку")
     elif message.text == "А зараз?":
         bot.send_message(message.from_user.id, "нет")
+
+    elif message.text == "1":
+        bot.send_message(message.from_user.id, "работає дуже криво")
     else:
         bot.send_message(message.from_user.id, "та перезапусти, та не вийде")
 
