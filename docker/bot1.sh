fd04ab141fa901131890f38370a317dddc3bce20
PassionSasha <sokolets.olexandr365@gmail.com>
2020-05-12 00:25:38 +0300

    sho39

--- docker/bot1.sh
+++ docker/bot1.sh
@@ -1,8 +1,6 @@
 
 rm -rf bot1 
 mkdir bot1
-git clone https://github.com/eternnoir/pyTelegramBotAPI.git
-cd pyTelegramBotAPI
-python setup.py install
+
 git clone --recursive https://github.com/PassionSasha/bot_lab.git
 cd /usr/local/bin/bot1 && git clone --recursive https://github.com/PassionSasha/bot_lab.git
