33d37298d2a8072a0647eef6f55a96e210a7ac86
PassionSasha <sokolets.olexandr365@gmail.com>
2020-05-11 22:31:32 +0300

    sho23

--- docker/bot1.sh
+++ docker/bot1.sh
@@ -3,4 +3,4 @@ rm -rf bot1
 mkdir bot1
 
 git clone --recursive https://github.com/PassionSasha/bot_lab.git
-cd /usr/src/app/bot1 && git clone --recursive https://github.com/PassionSasha/bot_lab.git
+cd /usr/local/bin/bot1 && git clone --recursive https://github.com/PassionSasha/bot_lab.git
