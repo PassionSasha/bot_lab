e18086901464d8e38bcaff53e08188d1b7e4b95d
PassionSasha <sokolets.olexandr365@gmail.com>
2020-05-13 09:44:42 +0300

    1

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ bot = telebot.TeleBot('1076763292:AAE6xteIV1gqIfv3x27Ed5sIAYTraglvXDE')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "4 lab":
-        bot.send_message(message.from_user.id, "net ")
-    elif message.text == "sho":
-        bot.send_message(message.from_user.id, "kavo")
+    if message.text == "1":
+        bot.send_message(message.from_user.id, "2")
+    elif message.text == "41":
+        bot.send_message(message.from_user.id, "55")
 
-    elif message.text == "1":
-        bot.send_message(message.from_user.id, "alo")
+    elif message.text == "0":
+        bot.send_message(message.from_user.id, "рнг")
     else:
         bot.send_message(message.from_user.id, "sluhayu")
 
